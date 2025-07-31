56dec717a9a3968ef67de6c2dc7bba796584e259
ashors1 <ashors@nvidia.com>
2025-07-30 22:04:27 -0700

    update documentation
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- examples/converters/convert_megatron_to_hf.py
+++ examples/converters/convert_megatron_to_hf.py
@@ -20,9 +20,9 @@ from nemo_rl.models.megatron.community_import import export_model_from_megatron
 
 """ NOTE: this script requires mcore. Make sure to launch with the mcore extra:
 uv run --extra mcore python examples/converters/convert_megatron_to_hf.py \
-  --config results/<path_to_chkp>/config.yaml \
-  --megatron-ckpt-path results/<path_to_chkp>/weights/iter_xxxxx \
-  --hf-ckpt-path results/<path_to_hf_chkp>/hf
+  --config <path_to_ckpt>/config.yaml \
+  --megatron-ckpt-path <path_to_ckpt>/policy/weights/iter_xxxxx \
+  --hf-ckpt-path <path_to_save_hf_ckpt>
 """
 
 
